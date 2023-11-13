2133ee954f0d9d28a4089f17aac36fc293818db1
Nthabiblossom <128476347+Nthabiblossom@users.noreply.github.com>
2023-11-13 14:33:38 +0200

    added weather search component

---
+++ src/WeatherSearch.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function WeatherSearch() {
+  return (
+    <div>
+      <form>
+        <input type="search" placeholder="enter a city" />
+        <button type="submit">search</button>
+      </form>
+    </div>
+  );
+}
