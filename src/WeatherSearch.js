983138bba4d19a934906690f16425da4891b49a0
Nthabiblossom <128476347+Nthabiblossom@users.noreply.github.com>
2023-11-13 15:16:48 +0200

    added github link

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -2,11 +2,20 @@ import React from "react";
 
 export default function WeatherSearch() {
   return (
-    <div>
+    <div className="Form">
       <form>
         <input type="search" placeholder="enter a city" />
         <button type="submit">search</button>
       </form>
+      <p>
+        <a
+          href="https://github.com/Nthabiblossom/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
     </div>
   );
 }
